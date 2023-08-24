4c43c3b207e7b040b4b3ef18cb3c9a654e3bbd4a
dev-pradnya <pradnya.includ@gmail.com>
2023-08-24 13:59:44 +0530

    chnages

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Join from './components/Join/Join';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 const App = () => {
+  console.log("hello")
   return (
     <Router>
       <Route path="/" exact component={Join} />
